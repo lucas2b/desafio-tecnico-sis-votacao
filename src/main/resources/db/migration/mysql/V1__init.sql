4109956f62bd0eee8dc4892bec1a8bef7b9f692b
Lucas Barros Bonine <lucasbonine@live.com>
2020-03-15 19:26:03 -0300

    alteracoes para base

--- src/main/resources/db/migration/mysql/V1__init.sql
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema sys_votacao
+-- Schema heroku_e40966382f520ff
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema sys_votacao
+-- Schema heroku_e40966382f520ff
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `sys_votacao` DEFAULT CHARACTER SET utf8 ;
-USE `sys_votacao` ;
+CREATE SCHEMA IF NOT EXISTS `heroku_e40966382f520ff` DEFAULT CHARACTER SET utf8 ;
+USE `heroku_e40966382f520ff` ;
 
 -- -----------------------------------------------------
--- Table `sys_votacao`.`associado`
+-- Table `heroku_e40966382f520ff`.`associado`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sys_votacao`.`associado` (
+CREATE TABLE IF NOT EXISTS `heroku_e40966382f520ff`.`associado` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NOT NULL,
   `cpf` VARCHAR(11) NOT NULL,
@@ -26,9 +26,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `sys_votacao`.`pauta`
+-- Table `heroku_e40966382f520ff`.`pauta`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sys_votacao`.`pauta` (
+CREATE TABLE IF NOT EXISTS `heroku_e40966382f520ff`.`pauta` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `descricao` VARCHAR(200) NULL,
   `inicio` DATETIME NULL,
@@ -40,9 +40,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `sys_votacao`.`votacao`
+-- Table `heroku_e40966382f520ff`.`votacao`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sys_votacao`.`votacao` (
+CREATE TABLE IF NOT EXISTS `heroku_e40966382f520ff`.`votacao` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `pauta_id` INT NOT NULL,
   `associado_id` INT NOT NULL,
@@ -52,12 +52,12 @@ CREATE TABLE IF NOT EXISTS `sys_votacao`.`votacao` (
   UNIQUE INDEX `unique_id_Pauta_id_Associado` (`pauta_id` ASC, `associado_id` ASC) VISIBLE,
   CONSTRAINT `fk_id_Pauta`
     FOREIGN KEY (`pauta_id`)
-    REFERENCES `sys_votacao`.`pauta` (`id`)
+    REFERENCES `heroku_e40966382f520ff`.`pauta` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_id_Associado`
     FOREIGN KEY (`associado_id`)
-    REFERENCES `sys_votacao`.`associado` (`id`)
+    REFERENCES `heroku_e40966382f520ff`.`associado` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
